537f55bfa63110c131b93f55615a6463f5dd4297
Ketki4988 <83367075+Ketki4988@users.noreply.github.com>
2026-07-11 16:48:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import google.generativeai as genai
+# import google.generativeai as genai
+from google import genai
 
 # Configure Gemini API
 genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
